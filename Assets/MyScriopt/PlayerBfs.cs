8da05e207ce577a96e85632fce2b75c96cc1282c
yuito118 <72675978+yuito118@users.noreply.github.com>
2021-12-05 17:56:27 +0900

    Add:dfsでゴールする

--- Assets/MyScriopt/PlayerBfs.cs
+++ Assets/MyScriopt/PlayerBfs.cs
@@ -2,18 +2,19 @@ using System;
 using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerBfs : MonoBehaviour
 {
     int row = 7;
     int col = 8;
-    int[] start = new int[] {2,2};
-    int[] goal = new int[] {4,5};
+    int[] start = new int[] {1,1};
+    int[] goal = new int[] {3,4};
     int[,] dist = new int[7, 8] { {0,0,0,0,0,0,0,0},
                                 {0,1,1,1,1,1,1,0},
                                 {0,1,0,0,0,0,0,0},
-                                {0,1,1,0,0,1,1,0},
+                                {0,1,1,0,1,1,1,0},
                                 {0,1,1,0,0,1,1,0},
                                 {0,0,1,1,1,1,1,0},
                                 {0,0,0,0,0,0,0,0}};
@@ -34,24 +35,29 @@ public class PlayerBfs : MonoBehaviour
     {
         var stack = new Stack<int[]>();
         stack.Push(this.start);
-        var visited = new Dictionary<int[], bool>();
+        var visited = new Dictionary<string, bool>();
+        visited.Add(string.Concat(this.start),true);
         var d = new int[,] { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
 
         while (stack.Count > 0)
         {
             int[] current = stack.Pop();
-            transform.position = new Vector3(current[0]-0.5f,0,current[1] - 0.5f);
-            if (current == goal){
-                Debug.Log("goal!!!");
+
+            if (current.SequenceEqual(this.goal))
+	        {
+                Debug.Log(current);
+                Debug.Log("goal");
+                transform.position = new Vector3(current[0] + 0.5f, 0, current[1] + 0.5f);
                 break;
 	        }
+
 
             for (int i = 0; i < d.GetLength(0); i++)
             {
                 int x = current[0] + d[i, 0];
                 int z = current[1] + d[i, 1];
                 int[] next = { x,z };
-                if (visited.ContainsKey(next))
+                if (visited.ContainsKey(string.Concat(next)))
                 {
                     continue;
 		        }
@@ -67,12 +73,9 @@ public class PlayerBfs : MonoBehaviour
 		        }
 
                 stack.Push(next);
-                visited.Add(next, true);
-                Thread.Sleep(1000);
+                visited.Add(string.Concat(next), true);
             }
-
         }
-        
     }
 
     bool IsOutside(int x, int z)
@@ -82,6 +85,6 @@ public class PlayerBfs : MonoBehaviour
 
     bool IsConflict(int x,int z)
     {
-        return Convert.ToBoolean(this.dist[x, z]);
+        return !Convert.ToBoolean(this.dist[x, z]);
     }
 }
